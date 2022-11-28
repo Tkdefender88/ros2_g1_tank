55a9456fb392c4fb347fdcacc53d2e8166ffd778
tkdefender88 <tkdefender@protonmail.com>
2022-11-28 21:22:12 +0000

    changes to the motor node

--- ros2_g1_tank/motorNode.py
+++ ros2_g1_tank/motorNode.py
@@ -1,17 +1,34 @@
 import rclpy
 from rclpy.node import Node
 from gpiozero import LED
+from gpiozero import AngularServo 
 
-LED_R = "22"
-LED_G = "27"
-LED_B = "24"
+LED_R = 22
+LED_G = 27
+LED_B = 24
+
+servo = AngularServo(23, min_angle=0, max_angle=90)
 
 class Motors(Node):
     def __init__(self):
         super().__init__("motors")
         self.get_logger().info("Say Hello")
-        led = LED(LED_R)
-        led.on()
+        self.red = LED(LED_R)
+        self.led = 0
+
+        self.create_timer(1, self.timer_callback)
+    def timer_callback(self):
+        if(self.led == 1):
+            self.red.on()
+            self.led = 0
+            self.setAngle = 0
+        else:
+            self.led = 1
+            self.red.off()
+            self.setAngle = 45
+
+    def setAngle(angle):
+        servo.angle = angle
 
 
 
